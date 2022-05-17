c648f9ce878e8f7c1ab72991c47cbad263ca691c
msiivone <miklas.siivonen@tietoevry.com>
2022-05-17 10:16:14 +0300

    test10

---
+++ terraform/test10.tf
@@ -0,0 +1,22 @@
+module "test10" {
+  source = "./modules/aft-account-request"
+  control_tower_parameters = {
+    AccountEmail              = "leibnitz9+test10@gmail.com"
+    AccountName               = "test10"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "miklas.siivonen@tietoevry.com"
+    SSOUserFirstName          = "Miklas"
+    SSOUserLastName           = "Siivonen"
+  }
+  account_tags = {
+    "Update deployment" = "AFT"
+  }
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+  custom_fields = {
+    group = "non-prod"
+  }
+  account_customizations_name = "test10"
+}
